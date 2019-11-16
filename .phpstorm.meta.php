93f47387dc8afbe0ac435a9d0e213f5baff6c242
Edward Pol <edpol@p4bh.org>
2019-11-16 09:46:08 -0500

    1st attempt to get rid of alerts

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-11-15 02:19:11.
+    * Generated on 2019-11-16 14:45:06.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
